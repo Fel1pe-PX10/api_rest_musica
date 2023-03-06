413429b6dc3b9fcb512e818b20adb29efbbd9799
Felipe Paez <ofpaez87@gmail.com>
2023-03-06 18:42:54 -0500

    Artist: Metodos getUser, listar, editar, eliminar

--- controllers/artist.js
+++ controllers/artist.js
@@ -1,4 +1,9 @@
 
+const fs                 = require('fs');
+const mongoose           = require('mongoose');
+const mongoosePagination = require('mongoose-pagination');
+const path               = require('path');
+
 const Artist = require('../models/artist');
 
 // ruta prueba
@@ -32,11 +37,216 @@ const save = async (req, res) => {
 
         throw new Error('Error guardando el artista');
     }
+}
+
+const getArtist = async (req, res) => {
+
+    // Obtener id de la url
+    const id = req.params.id;
+
+    // Valida si el id enviado es valido
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({
+            status: 'error',
+            message: 'No es un id valido'
+        });
+    }
+
+    // Find a la bbdd
+    try {
+        const artist = await Artist.findById(id);
+        
+        return res.json({
+            status: 'success',
+            message: 'Get artist',
+            artist
+        });
+    } catch (error) {
+        console.log(error);
+
+        throw new Error('Error consultando al artista');
+    }
+
+}
+
+const list = async (req, res) => {
+
+    // Pagina pasada por url
+    const page = (!req.params.page) ? 1 : req.params.page;
+
+    const itemsPerPage = 5;
+
+    // Find a la base y al contador
+    try {
+        const [total, artists] = await Promise.all([
+            Artist.countDocuments(),
+            Artist.find()
+                    .sort('name')
+                    .paginate(page, itemsPerPage)
+        ])
+    
+    
+        return res.json({
+            status: 'success',
+            message: 'List artist',
+            total,
+            artists
+        });
+        
+    } catch (error) {
+        console.log(error);
+
+        throw new Error('Error consultado el listado de artistas');
+    }
+}
+
+const update = async (req, res) => {
+
+    // Obtener id de la url
+    const id = req.params.id;
+
+    // Comprobar que es un id valido
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({
+            status: 'error',
+            message: 'No es un id valido'
+        });
+    }
+
+    // Obtener datos Body
+    const data = req.body;
+
+    // Buscar y actualizar datos artista
+    try {
+        const userUpdate = await Artist.findByIdAndUpdate(id, data, {new: true});
+    
+        return res.json({
+            status: 'success',
+            message: 'Put  artist',
+            userUpdate
+        });
+        
+    } catch (error) {
+        console.log(error);
+
+        throw new Error('Error actualizando al artista');
+    }
+}
+
+const remove = async (req, res) => {
+
+    // Obtener el id del artista de la url
+    const id = req.params.id;
+    
+    try {
+        // Consultar y eliminar artista
+        const artist = await Artist.findByIdAndRemove(id);
+
+        // Eliminar los albunes
+
+        // Eliminar canciones
+        
+        
+        return res.json({
+            status: 'success',
+            message: 'Delete  artist',
+            artist
+        });
+    } catch (error) {
+        console.log(error);
+
+        throw new Error('Error intentado eliminar el artista');
+    }
+}
+
+const upload = async(req, res) => {
+
+    // Obtener id de la url
+    const id = req.params.id;
+
+    // Comprobar que es un id valido
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({
+            status: 'error',
+            message: 'No es un id valido'
+        });
+    }
+    
+    // Obtener fichero de imagen
+    if(!req.file){
+        return res.status(404).json({
+            status: 'error',
+            message: 'La petición no incluye la imagen'
+        });
+    }
+
+    // Obtener el nombre de la imagen
+    const image = req.file.originalname;
+
+    // Obetener informacion de la imagen
+    const imageSplit = image.split('\.');
+
+    // Comprobar extension valida
+    const extension = imageSplit[imageSplit.length-1];
+    if(extension != 'png' && extension != 'jpeg' && extension != 'gif' && extension != 'jpg'){
+        const filePath = req.file.path;
+        const fileDelete = fs.unlinkSync(filePath);
+
+        return res.status(400).json({
+            status: 'error',
+            message: 'La extensión no es válida',
+        });
+    }
+
+    // Guardar imagen en ddbb
+    try {
+        const update = await Artist.findByIdAndUpdate({_id: id}, {image:req.file.filename}, {new:true});
+
+        if(update){
+            return res.json({
+                status: 'success',
+                message: 'Artist upload',
+                update
+            });
+        }
+        
+    } catch (error) {
+        console.log(error);
+
+        throw new Error('Error al momento de guardar el nombre de la imagen en la bbdd');
+    }
+}
+
+const image = async (req, res) => {
+    // Obtener parametro de la url
+    const file = req.params.file
+
+    //Montar el path real de la imagen
+    const filePath = './uploads/artists/'+file;
+
+    // Comprobar que existe el fichero
+    fs.stat(filePath, (error, exist) => {
+        if(error || !exist){
+            return res.status(404).json({
+                status: 'error',
+                message: 'No existe la imagen',
+            });
+        }
+
+        // devolver el fichero
+        return res.sendfile(path.resolve(filePath));
+    })
 
     
 }
 
 module.exports = {
+    getArtist,
+    list,
+    image,
     prueba,
-    save
+    remove,
+    save,
+    update,
+    upload
 }
--- routes/artist.js
+++ routes/artist.js
@@ -2,12 +2,33 @@ const express = require('express');
 
 const router = express.Router();
 
+const multer = require('multer');
+
+// Configuracion subida multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads/artists/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, 'artists-'+Date.now()+'-'+file.originalname)
+    }
+})
+// Middleware de uploads
+const updloads = multer({storage});
+
 const auth = require('../middlewares/auth');
 
 
-const { prueba, save } = require('../controllers/artist');
+const { prueba, save, getArtist, list, update, remove, upload, image } = require('../controllers/artist');
+
 
 router.get('/prueba', prueba);
 router.post('/save', [auth], save);
+router.get('/get/:id', [auth], getArtist);
+router.get('/list/:page?', [auth], list);
+router.put('/update/:id', [auth], update);
+router.delete('/remove/:id', [auth], remove);
+router.post('/upload/:id', [auth, updloads.single('file0')], upload);
+router.get('/image/:file', [auth], image);
 
 module.exports = router;
